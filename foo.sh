f0e14032505c60a3566ad0782d9714af5b17b4c4
Richard Houck <rdhouck@gmail.com>
2020-02-12 23:25:04 -0800

    udpated

--- foo.sh
+++
@@ -1,6 +0,0 @@
-echo "# group-todo" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/acanlord/group-todo.git
-git push -u origin master
