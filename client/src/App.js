7f813179278f8b60ff57b172bbc3e43bca1b09f3
Pauline Cheng <pauline.cheng1@gmail.com>
2020-02-24 20:10:26 -0800

    app is now 2 pages

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 import LandingPage from './components/pages/LandingPage/LandingPage.js';
 import Todo from './components/pages/Todo/Todo.js';
+import Blog from './components/pages/Blog/Blog.js';
 import Todocopy from './components/pages/Todo/Todo.js';
 
 // import TodoList from './components/pages/TodoList/TodoList.js';
@@ -16,25 +17,27 @@ class App extends Component {
     return (
       <div className="App">
         <nav className="App-navigation">
-          <h1 className="App-title">What's happening today?</h1>
+          <h1 className="App-title">Just do it</h1>
+          
           {/*<Link to="/">Welcome</Link>*/}
-          {/*<Link to="/todo/">Todo</Link>*/}
+          <Link to="/todo/">Todo</Link>
           {/* <Link to="/todolist/">TodoList</Link> */}
-          {/* <Link to="/blog/">Blog</Link> */}
+          <Link to="/blog/">Blog</Link>
           {/* <Link to="/write/">Write Article</Link> */}
         </nav>
+        <div>
+        <h2 >To Do, when there's nothing else.</h2>
+        </div>
 
         <div className="App-mainContent">
-          <Todo />
-          <Todocopy />
-          {/* <Switch> */}
+          <Switch> 
             {/* <Route exact path='/' component={LandingPage} /> */}
-            {/* <Route exact path='/todo/' component={Todo} /> */}
-            {/* <Route exact path='/todocopy/' component={Todocopy} /> */}
+            <Route exact path='/todo/' component={Todo} /> 
+            <Route exact path='/todocopy/' component={Todocopy} />
             {/* <Route exact path='/todolist/' component={TodoList} /> */}
-            {/* <Route exact path='/blog/' component={Blog} /> */}
+            <Route exact path='/blog/' component={Blog} />
             {/* <Route exact path='/write/' component={WriteArticle} /> */}
-          {/*</Switch>*/}
+          </Switch>
         </div>
 
       </div>
